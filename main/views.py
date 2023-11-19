ca4ae261505677eb527d8f400472fa8465b1f3d8
Aaron <aaron.kwek@ui.ac.id>
2023-11-19 22:31:39 +0700

    revisi model

--- main/views.py
+++ main/views.py
@@ -169,7 +169,7 @@ def create_product_flutter(request):
         new_product = Item.objects.create(
             user = request.user,
             name = data["name"],
-            price = int(data["price"]),
+            amount = int(data["amount"]),
             description = data["description"]
         )
 
